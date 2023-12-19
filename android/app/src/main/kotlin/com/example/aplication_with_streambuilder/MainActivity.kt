a7896e09c4cbf33ddd8aca00ee0bb9a37b20ad54
ca1ogabriel <caang.2022119minf06@aluno.ifpi.edu.br>
2023-12-19 09:50:36 -0300

    add stream builder

---
+++ android/app/src/main/kotlin/com/example/aplication_with_streambuilder/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.aplication_with_streambuilder
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:aplication_with_streambuilder/Stream_builder/stream_builder.dart';
+void main() {
+  runApp(const MyApp());
+}
