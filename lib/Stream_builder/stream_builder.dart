86c7dfc7a5a05496f45fdfedc20e1bf7a8be1f91
ca1ogabriel <caang.2022119minf06@aluno.ifpi.edu.br>
2023-12-20 08:48:51 -0300

    alterações feitas

--- lib/Stream_builder/stream_builder.dart
+++ lib/Stream_builder/stream_builder.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class MyApp extends StatefulWidget {
@@ -8,13 +10,72 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  StreamController<String> streamController = StreamController<String>();
+  late Stream<String> dataStream;
+  TextEditingController textEditingController = TextEditingController();
+
+  @override
+  void initState() {
+    dataStream = streamController.stream.asBroadcastStream();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Stream builder'),
-      ),
-      body: const Text('Hello'),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              StreamBuilder<String>(
+                stream: dataStream,
+                builder: (context, snapshot) {
+                  if(snapshot.hasData){
+                    return Text(snapshot.data ??'Null data',
+                  style: const TextStyle(
+                    fontSize: 30, fontWeight: FontWeight.bold
+                  ),);
+                  }
+                  else{
+                    return const Text('No data',
+                  style: TextStyle(
+                    fontSize: 30, fontWeight: FontWeight.bold
+                  ),);
+                  }
+                  
+                }
+              ),
+              StreamBuilder<String>(
+                stream: dataStream,
+                builder: (context, snapshot) {
+                  if(snapshot.hasData){
+                    return Text(snapshot.data ??'Null data',
+                  style: const TextStyle(
+                    fontSize: 30, fontWeight: FontWeight.bold
+                  ),);
+                  }
+                  else{
+                    return const Text('No data',
+                  style: TextStyle(
+                    fontSize: 30, fontWeight: FontWeight.bold
+                  ),);
+                  }
+                  
+                }
+              ),
+              const SizedBox(height: 20,),
+               SizedBox(width: 200,
+              child: TextField(
+                controller: textEditingController,
+              )),
+              ElevatedButton(onPressed: (){
+                streamController.add(textEditingController.text);
+              }, child: const Text('iniciar'))
+            ],
+          ),
+        ),  
+      )
     );
   }
 }
